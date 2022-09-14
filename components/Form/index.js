537523e8ff53632e96a44a0d18576f5317e3341a
phuong <phuongdenpro@gmail.com>
2022-09-14 14:33:16 +0700

    add

--- components/Form/index.js
+++ components/Form/index.js
@@ -5,18 +5,32 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
+  Keyboard,
 } from "react-native";
 import styles from "./style";
+import { useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
+  const [task, setTask] = useState('');
+  const handleAddTask = ()=>{
+    if(task.length === 0){
+      alert("Bạn vui lòng nhập công việc");
+      return;
+    }
+    props.onAddTask(task);
+    setTask('');
+    Keyboard.dismiss();
+  }
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       KeyboardAvoidingView = {10}
       style={styles.addTask}
     >
-      <TextInput style={styles.input} placeholder="Your task"></TextInput>
-      <TouchableOpacity onPress={}>
+      <TextInput style={styles.input} placeholder="Your task"
+        onChangeText={(text) =>setTask(text)} value ={task}
+      ></TextInput>
+      <TouchableOpacity onPress={handleAddTask}>
         <View style={styles.icon}>
           <Text style={styles.textIcon}>+</Text>
         </View>
