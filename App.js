5bac5657057e87b4292d17a6e8dd6c1a6cd5bd43
phuong <phuongdenpro@gmail.com>
2022-09-14 14:33:07 +0700

    add task

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 
 import {
@@ -8,26 +8,32 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
-import Task from "./components/Task/index";
+import Task from "./components/Task";
 import styles from "./App.components.style";
 import Form from "./components/Form";
 
 const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const handleAddTask = (task) => {
+    setTaskList([...taskList, task]);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.body}>
         <Text style={styles.header}>Todo List</Text>
         <ScrollView style={styles.items}>
-          <Task />
-          <Task />
+          {
+            taskList.map((item,index) => {
+              return <Task />
+            })
+          }
         </ScrollView>
       </View>
-      <Form />
+      <Form onAddTask={handleAddTask} />
 
       <StatusBar style="dark" />
     </View>
   );
-}
+};
+
 export default App;
-
-
